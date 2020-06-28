cbcdaefedb4cf79f0219d12abe542f5a867ebac1
Jakob Ondrey <ondrey@gmail.com>
2020-06-28 14:05:15 -0500

    prod

--- app/app.py
+++ app/app.py
@@ -3,15 +3,25 @@ import json
 
 client = boto3.client('dynamodb')
 
-def get_update_visit_counter(site_url):
+def get_update_visit_counter(event, context):
     response = client.update_item(
         TableName='jakobondrey_visitor_count',
         Key={
             'site_url': {
                 'S': 'jakobondrey.com'
-            },
+            }
         },
         ReturnValues='UPDATED_NEW',
-        UpdateExpression='SET count = count + 1',
+        UpdateExpression='SET visit_count = visit_count + :val',
+        ExpressionAttributeValues={
+            ":val": {
+                "N": "1"
+            }
+        }
     )
-    print(response)
+    visit_count = response["Attributes"]["visit_count"]["N"]
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"Visit_Count": visit_count})
+    }
+    
