7f4949aac6ea23938b339fe1f59219f0c21cc23c
Jakob Ondrey <ondrey@gmail.com>
2020-07-10 06:30:01 -0500

    push to test dynamo mockup in github actions

--- app/app.py
+++ app/app.py
@@ -2,28 +2,43 @@ import boto3
 import json
 import os
 
-client = boto3.client('dynamodb')
 TABLE_NAME = os.getenv("TABLE_NAME", "Fake_table")
+SITE_URL = os.getenv("SITE_URL", "fake_site.com")
+
+client = none
+    
+class UpdateItem(object):  
+    
+    def __init__(self, client):
+        self.client = client
+
+    def update_count(self):
+        response = client.update_item(
+            TableName=TABLE_NAME,
+            Key={
+                'site_url': {
+                    'S': SITE_URL
+                }
+            },
+            ReturnValues='UPDATED_NEW',
+            UpdateExpression='ADD visit_count :val',
+            ExpressionAttributeValues={
+                ":val": {
+                    "N": "1"
+                }
+            }
+        )
+        
+        visit_count = response["Attributes"]["visit_count"]["N"]
+        return {
+            "statusCode": 200,
+            "headers": {'Access-Control-Allow-Origin': '*'},
+            "body": json.dumps({"Visit_Count": visit_count})  # to access this in front end http call to api gateway endpoint.
+        }
 
 def get_update_visit_counter(event, context):
-    response = client.update_item(
-        TableName=TABLE_NAME,
-        Key={
-            'site_url': {
-                'S': 'jakobondrey.com'
-            }
-        },
-        ReturnValues='UPDATED_NEW',
-        UpdateExpression='ADD visit_count :val',
-        ExpressionAttributeValues={
-            ":val": {
-                "N": "1"
-            }
-        }
-    )
-    visit_count = response["Attributes"]["visit_count"]["N"]
-    return {
-        "statusCode": 200,
-        "headers": {'Access-Control-Allow-Origin': '*'},
-        "body": json.dumps({"Visit_Count": visit_count})  # to access this in front end http call to api gateway endpoint.
-    }
+    global client
+    if not client:
+        client = boto3.client('dynamodb')
+    obj = UpdateItem(client)
+    obj.update_count()
--- app/test_test.py
+++ app/test_test.py
@@ -1,11 +1,88 @@
 import json
-from app import get_update_visit_counter
+import os
+import boto3
 
-response1 = get_update_visit_counter({}, {})
-response2 = get_update_visit_counter({}, {})
+from app import UpdateItem
+from unittest import TestCase, mock
+
+client = boto3.client(
+    "dynamodb", 
+    endpoint_url="http://localhost:8000",
+    aws_access_key_id="ACCKEY111",
+    aws_secret_access_key="SECKEY999")
+
+TEST_TABLE_NAME = "Fake_table"
+TEST_SITE_URL = "fake_site.com"
+
+class DynamoTest(TestCase):
+    def setUp(self):
+        # Create table
+        client.create_table(
+            TableName=TEST_TABLE_NAME,
+            KeySchema=[
+                {
+                    "AttributeName": "site_url",
+                    "KeyType": "HASH"
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    "AttributeName": "site_url",
+                    "AttributeType": "S"
+                }
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+    def tearDown(self):
+        client.delete_table(
+            TableName=TEST_TABLE_NAME
+        )
+
+    @mock.patch('update_item.TABLE_NAME', TEST_TABLE_NAME)
+    @mock.patch('update_item.SITE_URL', TEST_SITE_URL)
+    def test_update(self):
+        test_obj = UpdateItem(client)
+        response = test_obj.update_count()
+        body_response = json.loads(response["body"])
+        # assert count was incremented by one
+        self.assertEqual(200, response['statusCode'])
+        # assert incremented by 1
+        self.assertEqual("1", body_response["Visit_Count"])
+
+    @mock.patch('update_item.TABLE_NAME', TEST_TABLE_NAME)
+    @mock.patch('update_item.SITE_URL', TEST_SITE_URL)
+    def test_update_when_already_exists(self):
+        test_obj = UpdateItem(client)
+        response = test_obj.update_count()
+        body_response = json.loads(response["body"])
+        # assert count was incremented by one
+        self.assertEqual(200, response['statusCode'])
+        # assert incremented by 1
+        self.assertEqual("1", body_response["Visit_Count"])
+
+        response2 = test_obj.update_count()
+        body_response2 = json.loads(response2["body"])
+        self.assertEqual("2", body_response2["Visit_Count"])
+
+    def test_fail(self):
+        self.assertEqual("2", "1")
 
 
-def test_response():
-    count1 = int(json.loads(response1["body"])["Visit_Count"])
-    count2 = int(json.loads(response2["body"])["Visit_Count"])
-    assert count2 - count1 == 1
+
+
+
+
+
+# response1 = get_update_visit_counter({}, {})
+# response2 = get_update_visit_counter({}, {})
+
+
+
+
+
+
+# def test_response():
+#     count1 = int(json.loads(response1["body"])["Visit_Count"])
+#     count2 = int(json.loads(response2["body"])["Visit_Count"])
+#     assert count2 - count1 == 1
